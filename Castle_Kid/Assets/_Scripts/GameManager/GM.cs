a017ed94eb8ac6e063574145c8ca18421f3b356d
Alexandre-Dollinger <alexandre.dollinger@epita.fr>
2025-05-03 14:11:00 +0200

    TryMultiplayer

--- Castle_Kid/Assets/_Scripts/GameManager/GM.cs
+++ Castle_Kid/Assets/_Scripts/GameManager/GM.cs
@@ -63,5 +63,7 @@ namespace _Scripts.GameManager
 
             return angle;
         }
+
+        public static bool GameStarted = false;
     }
 }
---
+++ Castle_Kid/Assets/_Scripts/Multiplayer/NetworkManagerScript.cs
@@ -0,0 +1,51 @@
+using System.Net;
+using System.Net.Sockets;
+using _Scripts.GameManager;
+using TMPro;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine;
+
+namespace _Scripts.Multiplayer
+{
+    public class NetworkManagerScript : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI ipAddressText;
+        [SerializeField] private TMP_InputField ip;
+        
+        private string _ipAddress;
+        private UnityTransport _transport;
+        
+        // To Host a game
+        public void StartHost() {
+            NetworkManager.Singleton.StartHost();
+            GetLocalIPAddress();
+            GM.GameStarted = true;
+        }
+        
+        // To Join a game
+        public void StartClient() {
+            _ipAddress = ip.text;
+            SetIpAddress();
+            NetworkManager.Singleton.StartClient();
+            GM.GameStarted = true;
+        }
+        
+        private void SetIpAddress() {
+            _transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+            _transport.ConnectionData.Address = _ipAddress;
+        }
+        
+        private string GetLocalIPAddress() {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress eachIP in host.AddressList) {
+                if (eachIP.AddressFamily == AddressFamily.InterNetwork) {
+                    ipAddressText.text = "IP : " + eachIP.ToString();
+                    _ipAddress = eachIP.ToString();
+                    return eachIP.ToString();
+                }
+            }
+            throw new System.Exception("No network adapters with an IPv4 address in the system!");
+        }
+    }
+}
