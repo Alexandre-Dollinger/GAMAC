21438efd7e5e72166975a7e964a802bed7a6467a
Your Name <you@example.com>
2025-05-16 23:41:25 +0200

    addingShakeCamtoFollowScript

--- Castle_Kid/Assets/_Scripts/Camera/FollowScript.cs
+++ Castle_Kid/Assets/_Scripts/Camera/FollowScript.cs
@@ -36,4 +36,61 @@ public class FollowScript : NetworkBehaviour
         newPos = Vector3.Slerp(new Vector3(newPos.x, newPos.y, -10), new Vector3(posParent.x, posParent.y, -10), cameraSpeed * Time.deltaTime);
         transform.position = new Vector3(newPos.x, newPos.y + yOffset, -10);
     }
+    
+    //ShakingCam
+    //(https://youtu.be/iqujFS8mMcE?si=35MAj9QAxxR_2zhp)
+    
+    private Vector3 originalPos;
+    private bool shaking = false;
+    
+    
+    void Start()
+    {
+        originalPos = transform.localPosition;
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShakeCamera(0.5f, 0.05f, true, true);
+        }
+    }
+
+    public void ShakeCamera(float duration, float strength, bool horizontal, bool vertical)
+    {
+        if (shaking)
+        {
+            return;
+        }
+        StartCoroutine(Shake(duration, strength, horizontal, vertical));
+    }
+
+    private IEnumerator Shake(float duration, float strength, bool horizontal, bool vertical)
+    {
+        originalPos = transform.localPosition;
+        shaking = true;
+
+        while (duration > 0)
+        {
+            Vector3 ShakeOffset = Vector3.zero;
+
+            if (horizontal)
+            {
+                ShakeOffset.x = Random.Range(-1f, 1f) * strength;
+            }
+
+            if (vertical)
+            {
+                ShakeOffset.y = Random.Range(-1f, 1f) * strength;
+            }
+            
+            transform.localPosition = originalPos + ShakeOffset;
+            duration -= Time.deltaTime;
+            yield return null;
+        }
+        
+        transform.localPosition = originalPos;
+        shaking = false;
+    }
 }
